31d805595513199d7f28aa59ac48aeb3450c98a5
pravinbhagwat <pravinbhagwat150@gmail.com>
2023-06-27 16:17:21 +0530

    submitting code-Tue Jun 27 2023 16:17:21 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+const [numbers, setNumbers] = useState([]);
+const [sum, setSum] = useState(0);
+
+function calculateSum(e) {
+const inputValue = +e.target.value;
+
+setNumbers((prevNumbers) => [...prevNumbers, inputValue]);
 }
 
-export default App
+useEffect(() => {
+const calculateSumAsync = async () => {
+const newSum = numbers.reduce((acc, curr) => acc + curr, 0);
+setSum(newSum);
+};
+
+calculateSumAsync();
+}, [numbers]);
+
+return (
+<div>
+<h1>Sum Calculator</h1>
+<input type="number" onChange={calculateSum} />
+<p>Sum: {sum}</p>
+</div>
+);
+};
+
+export default App;
